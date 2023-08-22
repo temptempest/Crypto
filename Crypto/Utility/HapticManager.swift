dad2ad7a3e54b578ed2a3f9c7961b1025d2cd274
Victor <98951159+temptempest@users.noreply.github.com>
2023-08-22 18:46:55 +0400

    Update HapticManager.swift

--- Crypto/Utility/HapticManager.swift
+++ Crypto/Utility/HapticManager.swift
@@ -12,7 +12,6 @@ import SwiftUI
 
 class HapticManager {
     static private let generator = UINotificationFeedbackGenerator()
-    
     static func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
         generator.notificationOccurred(type)
     }
